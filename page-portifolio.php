6cfd0a1c457ae3ffb40260982266c012f0087fa4
Lucas J Costa <lucjoc.1@hotmail.com>
2025-07-26 16:52:10 -0300

    Iniciado portifolio mobile

--- page-portifolio.php
+++ page-portifolio.php
@@ -61,7 +61,12 @@ $current_page_url = get_permalink();
                     sort($unique_titles); ?>
 
                     <button type="button" class="client-filter-toggle">
-                        <span class="client-filter-current">Filtrar por Clientes</span>
+                        <span class="client-filter-current"><?php echo pll_e('Filtrar por Clientes') ?></span>
+                        <span class="client-filter-icon-mobile">
+                            <span class="line line1"></span>
+                            <span class="line line2"></span>
+                            <span class="line line3"></span>
+                        </span>
                     </button>
                     <ul class="client-filter-list">
                         <li data-client="" class="active"> <span>Filtrar por Clientes</span> </li>
@@ -83,6 +88,7 @@ $current_page_url = get_permalink();
                         foreach ($termos_portifolio as $termo_obj) {
                             $term_slug_for_data = isset($termo_obj["categoria_do_termo"]) ? $termo_obj["categoria_do_termo"] : '';
                             $term_name_for_display = isset($termo_obj['termo']) ? $termo_obj['termo'] : 'Termo Inválido';
+                            $term_name_for_display_mobile = isset($termo_obj['termo_mobile']) ? $termo_obj['termo_mobile'] : 'Termo Inválido';
 
                             // Adiciona a classe 'active-term' se este for o termo selecionado inicialmente (vindo da URL)
                             $is_active_class = ($selected_term_slug === $term_slug_for_data && !empty($selected_term_slug)) ? 'active-term' : '';
@@ -94,6 +100,8 @@ $current_page_url = get_permalink();
                                         data-term-slug="<?php echo esc_attr($term_slug_for_data); ?>">
                                         <span
                                             class="btn-portifolio-terms-text"><?php echo esc_html($term_name_for_display); ?></span>
+                                        <span
+                                            class="btn-portifolio-terms-text-mobile"><?php echo esc_html($term_name_for_display_mobile); ?></span>
                                     </a>
                                 </div>
                             </div>
@@ -104,116 +112,91 @@ $current_page_url = get_permalink();
                     }
                     ?>
                 </div>
-                <div class="portifolio-terms-content-mobile">
-                    <div class="owl-portifolio-terms owl-carousel owl-theme">
-                        <?php
-                        if (is_array($termos_portifolio) && count($termos_portifolio) > 0) {
-                            foreach ($termos_portifolio as $termo_obj) {
-                                $term_slug_for_data = isset($termo_obj["categoria_do_termo"]) ? $termo_obj["categoria_do_termo"] : '';
-                                $term_name_for_display = isset($termo_obj['termo']) ? $termo_obj['termo'] : 'Termo Inválido';
-
-                                // Adiciona a classe 'active-term' se este for o termo selecionado inicialmente (vindo da URL)
-                                $is_active_class = ($selected_term_slug === $term_slug_for_data && !empty($selected_term_slug)) ? 'active-term' : '';
-                                ?>
-                                <div class="portifolio-terms-item">
-                                    <div class="portifolio-terms-item-button">
-                                        <a href="javascript:void(0);"
-                                            class="btn-portifolio-terms <?php echo esc_attr($is_active_class); ?>"
-                                            data-term-slug="<?php echo esc_attr($term_slug_for_data); ?>">
-                                            <span
-                                                class="btn-portifolio-terms-text"><?php echo esc_html($term_name_for_display); ?></span>
-                                        </a>
-                                    </div>
-                                </div>
-                                <?php
-                            }
-                        } else {
-                            echo '<p>' . esc_html__('No portfolio terms available', 'text-domain') . '</p>';
-                        }
-                        ?>
-                    </div>
-                </div>
             </div>
         </div>
 
-        <?php
-        // --- LÓGICA PARA EXIBIR POSTS DO PORTFÓLIO (pag_portifolio) ---
-        $prioritized_posts_output = [];
-        $other_posts_output = [];
 
-        $args_all_posts = array(
-            'post_type' => 'pag_portifolio',
-            'lang' => $current_lang,
-            'posts_per_page' => -1,
-            'orderby' => 'date',
-            'order' => 'DESC',
-        );
-
-        $all_posts_query = new WP_Query($args_all_posts);
-
-        if ($all_posts_query->have_posts()) {
-            while ($all_posts_query->have_posts()) {
-                $all_posts_query->the_post();
-                $post_object = get_post();
-
-
-                $esta_no_termo = false;
-                if (!empty($selected_term_slug)) {
-                    $esta_no_termo = has_term($selected_term_slug, 'category', $post_object->ID);
-                }
-
-
-                if ($esta_no_termo) {
-                    $prioritized_posts_output[] = $post_object;
-                } else {
-                    $other_posts_output[] = $post_object;
-                }
-            }
-            wp_reset_postdata();
-        }
-
-        $final_posts_to_display = array_merge($prioritized_posts_output, $other_posts_output);
-
-        if (!empty($final_posts_to_display)) {
-            echo '<div class="portfolio-items-container">';
-            $index = 0;
-            foreach ($final_posts_to_display as $post_item) {
-                $post_item = get_post($post_item->ID);
-                $post_title = $post_item->post_title;
-                $post_content = $post_item->post_content;
-                $post_img_highlight = get_field('imagem_destaque', $post_item->ID);
-                $post_img_modal_highlight = get_field('imagem_destaque_modal', $post_item->ID);
-                $post_data = get_field('data_execucao', $post_item->ID);
-                $post_category = get_the_terms($post_item->ID, 'category');
-
-                ?>
-                <div class="portfolio-item" arial-index="<?php echo esc_attr($index); ?>">
-                    <div class="portifolio-img-highlight">
-                        <picture>
-                            <source src="<?php echo $post_img_highlight ?>">
-                            <img src="<?php echo esc_url($post_img_highlight); ?>" alt="<?php echo esc_attr($post_title); ?>">
-                        </picture>
-                    </div>
-                    <div class="portifolio-btn-modal" data-index="<?php echo esc_attr($index); ?>">
-                        <button>
-                            <img class="light" src="<?php echo get_template_directory_uri(); ?>/assets/img/portifolio-arrow.svg"
-                                alt="" srcset="">
-                            <img class="dark"
-                                src="<?php echo get_template_directory_uri(); ?>/assets/img/portifolio-arrow-white.svg" alt=""
-                                srcset="">
-                        </button>
-                    </div>
-                </div>
-                <?php
-                $index++;
-            }
-            echo '</div>';
-        } else {
-            echo '<p>' . esc_html__('Nenhum item de portfólio encontrado.', 'text-domain') . '</p>';
-        }
-        ?>
     </div>
 </section>
+
+<section class="portifolio-items">
+    <?php
+    // --- LÓGICA PARA EXIBIR POSTS DO PORTFÓLIO (pag_portifolio) ---
+    $prioritized_posts_output = [];
+    $other_posts_output = [];
+
+    $args_all_posts = array(
+        'post_type' => 'pag_portifolio',
+        'lang' => $current_lang,
+        'posts_per_page' => -1,
+        'orderby' => 'date',
+        'order' => 'DESC',
+    );
+
+    $all_posts_query = new WP_Query($args_all_posts);
+
+    if ($all_posts_query->have_posts()) {
+        while ($all_posts_query->have_posts()) {
+            $all_posts_query->the_post();
+            $post_object = get_post();
+
+            $esta_no_termo = false;
+            if (!empty($selected_term_slug)) {
+                $esta_no_termo = has_term($selected_term_slug, 'category', $post_object->ID);
+            }
+
+            if ($esta_no_termo) {
+                $prioritized_posts_output[] = $post_object;
+            } else {
+                $other_posts_output[] = $post_object;
+            }
+        }
+        wp_reset_postdata();
+    }
+
+    $final_posts_to_display = array_merge($prioritized_posts_output, $other_posts_output);
+
+    if (!empty($final_posts_to_display)) {
+        echo '<div class="portfolio-items-container container">';
+        $index = 0;
+        foreach ($final_posts_to_display as $post_item) {
+            $post_item = get_post($post_item->ID);
+            $post_title = $post_item->post_title;
+            $post_content = $post_item->post_content;
+            $post_img_highlight = get_field('imagem_destaque', $post_item->ID);
+            $post_img_modal_highlight = get_field('imagem_destaque_modal', $post_item->ID);
+            $post_data = get_field('data_execucao', $post_item->ID);
+            $post_category = get_the_terms($post_item->ID, 'category');
+
+            ?>
+            <div class="portfolio-item" arial-index="<?php echo esc_attr($index); ?>">
+                <div class="portifolio-img-highlight">
+                    <picture>
+                        <source src="<?php echo $post_img_highlight ?>">
+                        <img src="<?php echo esc_url($post_img_highlight); ?>" alt="<?php echo esc_attr($post_title); ?>">
+                    </picture>
+                </div>
+                <div class="portifolio-btn-modal" data-index="<?php echo esc_attr($index); ?>">
+                    <button>
+                        <img class="light" src="<?php echo get_template_directory_uri(); ?>/assets/img/portifolio-arrow.svg"
+                            alt="" srcset="">
+                        <img class="dark"
+                            src="<?php echo get_template_directory_uri(); ?>/assets/img/portifolio-arrow-white.svg" alt=""
+                            srcset="">
+                    </button>
+                </div>
+            </div>
+            <?php
+            $index++;
+        }
+        echo '</div>';
+    } else {
+        echo '<p>' . esc_html__('Nenhum item de portfólio encontrado.', 'text-domain') . '</p>';
+    }
+    ?>
+</section>
+
+
 
 
 <div id="portfolio-modals-container">
